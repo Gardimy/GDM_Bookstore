12c5e5bf58d7d23b458c1c98911922d7ce04298f
Gardimy Charles <gardimycharles889@gmail.com>
2023-06-17 09:03:54 -0400

    Create the setup react and initialize the project

---
+++ src/components/Book.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Book() {
+  return (
+    <div className="Bookcontainer">
+      Action
+      <h3>The Hunger Games</h3>
+      <p>Suzanne Collins</p>
+      <ul>
+        <li>Delete</li>
+        <li>Delete</li>
+        <li>Delete</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Book;
