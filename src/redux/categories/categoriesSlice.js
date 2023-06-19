ee7e07563cce81404d50ca99eef40337dcc5c24a
Gardimy Charles <gardimycharles889@gmail.com>
2023-06-19 17:00:45 -0400

    create Setup Redux store

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = 'construction';
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+  },
+});
+
+export default categoriesSlice;
