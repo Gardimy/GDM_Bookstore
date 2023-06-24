62468975db1c4b49cf74476e96eb1036b48b6762
Gardimy Charles <gardimycharles889@gmail.com>
2023-06-23 21:56:33 -0400

    Aupdate BookForm.js file

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,21 +1,24 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { v4 as uuid } from 'uuid';
 import { addBook } from '../redux/books/booksSlice';
 
 const BookForm = () => {
   const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
-
+  const id = uuid();
+  const category = 'Fiction';
   const handleSubmit = (e) => {
     e.preventDefault();
     if (title.trim() && author.trim()) {
-      dispatch(addBook({ title, author }));
+      dispatch(addBook({
+        title, author, category, item_id: id,
+      }));
       setTitle('');
       setAuthor('');
     }
   };
-
   return (
     <div>
       <h2>Add New Book</h2>
@@ -37,5 +40,4 @@ const BookForm = () => {
     </div>
   );
 };
-
 export default BookForm;
